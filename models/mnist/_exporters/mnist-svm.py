64eefb654eac6f8ad4d2b8f2b42191a7c6be61d1
Marco Melis <marco.melis@unica.it>
2020-06-29 16:26:39 +0200

    #3 Now using simple `CClassifierSVM` for `mnist-svm` model as it natively supports multiclass.

---
+++ models/mnist/_exporters/mnist-svm.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.insert(0, '../../')
+
+from svm import svm
+from secml.data.loader import CDataLoaderMNIST
+
+
+tr = CDataLoaderMNIST().load('training')
+clf = svm()
+clf.n_jobs = 1
+clf.verbose = 1
+
+clf.C = 0.1
+
+tr.X /= 255
+
+clf.fit(tr.X, tr.Y)
+
+state_path = '../mnist-svm.gz'
+clf.save_state(state_path)
+
+import os
+print("Model stored into: " + os.path.abspath(state_path))
+
+from hashlib import md5
+md5_hash = md5()
+a_file = open(state_path, "rb")
+content = a_file.read()
+md5_hash.update(content)
+
+print('md5: ' + md5_hash.hexdigest())
